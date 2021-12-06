eb9f9c03f860bd97a07f509a7ef48b734d5cbece
Seunghyuk <seunghyukoh0@gmail.com>
2021-12-06 13:00:02 +0900

    Update - Add a package: blockchain

---
+++ blockchain/blockchain.go
@@ -0,0 +1,61 @@
+package blockchain
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"sync"
+)
+
+type Block struct {
+	Data     string
+	Hash     string
+	PrevHash string
+}
+
+type blockChain struct {
+	blocks []*Block
+}
+
+var b *blockChain
+var once sync.Once
+
+func getLastHash() string {
+	totalBlocks := len(GetBlockChain().blocks)
+	if totalBlocks == 0 {
+		return ""
+	}
+	return GetBlockChain().blocks[totalBlocks-1].Hash
+}
+
+func (b *Block) calculateHash() {
+	data := b.Data
+	prevHash := b.PrevHash
+
+	hash := sha256.Sum256([]byte(data + prevHash))
+
+	b.Hash = fmt.Sprintf("%x", hash)
+}
+
+func createBlock(data string) *Block {
+	newBlock := Block{Data: data, Hash: "", PrevHash: getLastHash()}
+	newBlock.calculateHash()
+	return &newBlock
+}
+
+func (b *blockChain) AddBlock(data string) {
+	b.blocks = append(b.blocks, createBlock(data))
+}
+
+func GetBlockChain() *blockChain {
+	if b == nil {
+		once.Do(func() {
+			b = &blockChain{}
+			b.AddBlock("Genesis Block")
+		})
+	}
+	return b
+}
+
+func (b *blockChain) AllBlocks() []*Block {
+	return b.blocks
+}
