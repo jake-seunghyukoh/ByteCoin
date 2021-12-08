34dfb3af7ca185f42f67c7aa9f3d0faee7047f17
Seunghyuk <seunghyukoh0@gmail.com>
2021-12-08 15:01:04 +0900

    Update - Minimal cli

---
+++ cli/cli.go
@@ -0,0 +1,46 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/ohshyuk5/ByteCoin/explorer"
+	"github.com/ohshyuk5/ByteCoin/rest"
+)
+
+func usage() {
+	fmt.Println("Welcome to Byte Coin")
+	fmt.Println()
+	fmt.Println("Please use the following flags:")
+	fmt.Println()
+	fmt.Println("-port:		Set port of the server")
+	fmt.Println("-mode:		Choose between 'html' and 'rest'")
+	fmt.Println()
+	os.Exit(0)
+}
+
+func Start() {
+
+	if len(os.Args) == 1 {
+		usage()
+	}
+
+	port := flag.Int("port", 4000, "Set port of the server")
+	mode := flag.String("mode", "rest", "Choose between 'html' and 'rest'")
+
+	flag.Parse()
+
+	switch *mode {
+	case "rest":
+		// start rest api
+		rest.Start(*port)
+	case "html":
+		// start html explorer
+		explorer.Start(*port)
+	default:
+		usage()
+	}
+
+	fmt.Println(*port, *mode)
+}
--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
-import (
-	"github.com/ohshyuk5/ByteCoin/rest"
-)
+import "github.com/ohshyuk5/ByteCoin/cli"
 
 func main() {
-	rest.Start(4000)
+	cli.Start()
 }
