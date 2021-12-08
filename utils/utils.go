c092d55a31c47eeffea6b3cd0f450a6c0f93758a
Seunghyuk <seunghyukoh0@gmail.com>
2021-12-08 21:09:51 +0900

    Update - Add util functions - Two functions for encoding from structure to byte and vice versa

--- utils/utils.go
+++ utils/utils.go
@@ -1,9 +1,25 @@
 package utils
 
-import "log"
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+)
 
 func HandleErr(err error) {
 	if err != nil {
 		log.Panic(err)
 	}
+}
+
+func ToBytes(i interface{}) []byte {
+	var blockBuffer bytes.Buffer
+	encoder := gob.NewEncoder(&blockBuffer)
+	HandleErr(encoder.Encode(i))
+	return blockBuffer.Bytes()
+}
+
+func FromBytes(i interface{}, data []byte) {
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+	HandleErr(decoder.Decode(i))
 }
