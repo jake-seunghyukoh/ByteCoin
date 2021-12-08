8248ce62196cd599bc939a9a1b3c3f8f88124c94
Seunghyuk <seunghyukoh0@gmail.com>
2021-12-08 21:08:15 +0900

    Update - Add new package db - This package uses bolt

---
+++ db/db.go
@@ -0,0 +1,78 @@
+package db
+
+import (
+	"fmt"
+
+	"github.com/boltdb/bolt"
+	"github.com/ohshyuk5/ByteCoin/utils"
+)
+
+const (
+	dbName        = "blockchain.db"
+	dataBucket    = "data"
+	blocksBucket  = "blocks"
+	blockchainKey = "checkpoint"
+)
+
+var db *bolt.DB
+
+func DB() *bolt.DB {
+	if db == nil {
+		dbPointer, err := bolt.Open(dbName, 0600, nil)
+		utils.HandleErr(err)
+		db = dbPointer
+		err = db.Update(func(t *bolt.Tx) error {
+			_, err := t.CreateBucketIfNotExists([]byte(dataBucket))
+			utils.HandleErr(err)
+
+			_, err = t.CreateBucketIfNotExists([]byte(blocksBucket))
+			return err
+		})
+		utils.HandleErr(err)
+	}
+	return db
+}
+
+func Close() {
+	err := DB().Close()
+	utils.HandleErr(err)
+}
+
+func SaveBlock(hash string, data []byte) {
+	fmt.Printf("Saving Block %s\nData: %b", hash, data)
+	err := DB().Update(func(t *bolt.Tx) error {
+		bucket := t.Bucket([]byte(blocksBucket))
+		err := bucket.Put([]byte(hash), data)
+		return err
+	})
+	utils.HandleErr(err)
+}
+
+func SaveBlockchain(data []byte) {
+	err := DB().Update(func(t *bolt.Tx) error {
+		bucket := t.Bucket([]byte(dataBucket))
+		err := bucket.Put([]byte(blockchainKey), data)
+		return err
+	})
+	utils.HandleErr(err)
+}
+
+func Blockchain() []byte {
+	var data []byte
+	DB().View(func(t *bolt.Tx) error {
+		bucket := t.Bucket([]byte(dataBucket))
+		data = bucket.Get([]byte(blockchainKey))
+		return nil
+	})
+	return data
+}
+
+func Block(hash string) []byte {
+	var data []byte
+	DB().View(func(t *bolt.Tx) error {
+		bucket := t.Bucket([]byte(blocksBucket))
+		data = bucket.Get([]byte(hash))
+		return nil
+	})
+	return data
+}
