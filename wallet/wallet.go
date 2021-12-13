de91d6b12b1dd6971a810580bc9c2a6a2adc1d28
Seunghyuk <seunghyukoh0@gmail.com>
2021-12-13 16:29:38 +0900

    Update - Verify a hash!

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -2,25 +2,38 @@ package wallet
 
 import (
 	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
+	"crypto/x509"
 	"encoding/hex"
 	"fmt"
 	"github.com/ohshyuk5/ByteCoin/utils"
+	"math/big"
 )
 
+const hashedMessage = "185f8db32271fe25f561a6fc938b2e264306ec304eda518007d1764826381969"
+const privateKey = "30770201010420efda9719cf88346df34dc48c7b88befcf9399cd2b08e6cf398390dc74bec0297a00a06082a8648ce3d030107a14403420004dba86a5a6472e12b5a39ce409a552d4c5858363256f2dd5bb834698a03f9c06db997c1c202ff028ce48965f45985594d0d2098a73bcefefde04cb70ad2c54ba3"
+const signature = "81fedf5207ca7fe8d74f944557a1bbd69866c77c18c138335fa1e1ab53ce0209521f083d9ee78321e80a92faf1d9d4082fd3d1fc83a71be4ab1c356a28749190"
+
 func Start() {
-	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	privateKeyAsBytes, err := hex.DecodeString(privateKey)
 	utils.HandleErr(err)
 
-	message := "Hello"
-	hashedMessage := utils.Hash(message)
-
-	hashedBytes, err := hex.DecodeString(hashedMessage)
+	restoredKey, err := x509.ParseECPrivateKey(privateKeyAsBytes)
 	utils.HandleErr(err)
 
-	r, s, err := ecdsa.Sign(rand.Reader, privateKey, hashedBytes)
+	signatureAsBytes, err := hex.DecodeString(signature)
 	utils.HandleErr(err)
 
-	fmt.Println(r, s)
+	rBytes := signatureAsBytes[:len(signatureAsBytes)/2]
+	sBytes := signatureAsBytes[len(signatureAsBytes)/2:]
+
+	var bigR, bigS = big.Int{}, big.Int{}
+
+	bigR.SetBytes(rBytes)
+	bigS.SetBytes(sBytes)
+
+	hashAsBytes, err := hex.DecodeString(hashedMessage)
+	utils.HandleErr(err)
+
+	ok := ecdsa.Verify(&restoredKey.PublicKey, hashAsBytes, &bigR, &bigS)
+	fmt.Println(ok)
 }
